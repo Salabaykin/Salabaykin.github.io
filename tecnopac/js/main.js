8b674d81ba45d439eb462310b56273195d288770
Евгений <57830696+Salabaykin@users.noreply.github.com>
2020-05-19 17:39:33 +0300

    Адаптив tecnopac

--- tecnopac/js/main.js
+++ tecnopac/js/main.js
@@ -29,11 +29,11 @@ const func = {
         });
       },
       scrollNav: (event) => {
-        if (window.scrollY > 100) {
+        if (window.scrollY > 50) {
           header.style.backgroundColor = '#05797a';
           headerWrapper.style = 'height: 80px;';
           logo.style = 'width: 160px; display: flex; align-items: center; transition: 0.3s ease';
-        } else if (window.scrollY < 100) {
+        } else if (window.scrollY < 50) {
           header.style.backgroundColor = '';
           headerWrapper.style = '';
           logo.style = '';
@@ -45,3 +45,45 @@ burgerButton.addEventListener('click', func.burgerButton);
 window.addEventListener('click', func.win);
 burgerLink.forEach(func.burgerLink);
 document.addEventListener('scroll', func.scrollNav);
+
+$(document).ready(function(){
+  $('.slick-slider__business').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slick-slider__business--img-prev"></div>',
+    nextArrow: '<div class="slick-slider__business--img-next"></div>',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false,
+    waitForAnimate: false,
+    responsive: [
+      {
+        breakpoint: 1070,
+        settings: {
+          arrows: false,
+          pauseOnHover: false,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          pauseOnHover: false,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+$(document).ready(function(){
+  $('.slick-slider__production').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: '<div class="slick-slider__production--img-prev"></div>',
+    nextArrow: '<div class="slick-slider__production--img-next"></div>',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
