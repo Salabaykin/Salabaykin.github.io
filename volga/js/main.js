2f6c17f346059a9468b2f1dd6fc4e1c5abc60175
Евгений <57830696+Salabaykin@users.noreply.github.com>
2020-06-01 18:04:43 +0300

    volga fix

--- volga/js/main.js
+++ volga/js/main.js
@@ -1,300 +1,23 @@
-$(document).ready(function() {
-
-$('.tariffs-slider').slick({
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  swipe: false,
-  prevArrow: '<div class="foundation-slider-prev"><svg class="foundation-slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  nextArrow: '<div class="foundation-slider-next"><svg class="foundation-slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  arrows: false,
-  focusOnSelect: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false,
-        arrows: false
-      }
-    },
-    {
-      breakpoint: 575,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false,
-        arrows: true
-      }
-    }
-  ]
-});
-
-$('.tariffs-slider-tabs').slick({
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  arrows: false,
-  asNavFor: '.tariffs-slider',
-  focusOnSelect: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: "unslick"
-    }
-  ]
-});
-
-
-$('.cases-slider').slick({
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  prevArrow: '<div class="cases-slider__prev"><svg class="cases-slider__icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  nextArrow: '<div class="cases-slider__next"><svg class="cases-slider__icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  responsive: [
-    {
-      breakpoint: 1335,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false,
-        arrows: false
-      }
-    }
-  ]
-});
-
-$('.calculator-form__slider').slick({
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  prevArrow: '<div class="calculator-form__slider-prev"><svg class="calculator-form__slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  nextArrow: '<div class="calculator-form__slider-next"><svg class="calculator-form__slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  responsive: [
-    {
-      breakpoint: 550,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: false
-      }
-    },
-    {
-      breakpoint: 350,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: false
-      }
-    }
-  ]
-});
-
-$('.stages-slider').slick({
-  infinite: false,
-  speed: 300,
-  slidesToShow: 1,
-  swipe: false,
-  arrows: false,
-  focusOnSelect: true
-});
-
-$('.stages-slider-tabs').slick({
-  slidesToShow: 6,
-  slidesToScroll: 1,
-  arrows: false,
-  asNavFor: '.stages-slider',
-  focusOnSelect: true,
-  infinite: false,
-  responsive: [
-    {
-      breakpoint: 1110,
-      settings: {
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        infinite: false
-      }
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        infinite: false
-      }
-    },
-    {
-      breakpoint: 850,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: false
-      }
-    },
-    {
-      breakpoint: 530,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: false
-      }
-    }
-  ]
-});
-
-$('.feedback-slider').slick({
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  prevArrow: '<div class="feedback-slider-prev"><svg class="feedback-slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  nextArrow: '<div class="feedback-slider-next"><svg class="feedback-slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  responsive: [
-    {
-      breakpoint: 950,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 800,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 575,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false
-      }
-    }
-  ]
-});
-
-$('.certificates-slider').slick({
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  prevArrow: '<div class="certificates-slider-prev"><svg class="certificates-slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  nextArrow: '<div class="certificates-slider-next"><svg class="certificates-slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  responsive: [
-    {
-      breakpoint: 950,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 800,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 575,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false
-      }
-    }
-  ]
-});
-
-$('.foundation-slider').slick({
-  slidesToShow: 3,
-  prevArrow: '<div class="foundation-slider-prev"><svg class="foundation-slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  nextArrow: '<div class="foundation-slider-next"><svg class="foundation-slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 575,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false,
-        arrows: true 
-      }
-    }
-  ]
-});
-
-$('.sale-slider').slick({
-  slidesToShow: 3,
-  prevArrow: '<div class="foundation-slider-prev"><svg class="foundation-slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  nextArrow: '<div class="foundation-slider-next"><svg class="foundation-slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 575,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: false,
-        dots: false,
-        arrows: true 
-      }
-    }
-  ]
-});
-
-});
-
 const burger = document.querySelector('.burger'),
       popupClose = document.querySelector('.popup-close'),
       menu = document.querySelector('.header__menu'),
       navigationLink = document.querySelectorAll('.navigation__link'),
       up = document.querySelector('.up');
 
-
 const func = {
-  toggleMenu: () => {
+  toggleMenu: function() {
     menu.classList.toggle('menu-active');
     popupClose.classList.toggle('menu-active');
   },
-  navLink: (el) => {
-    el.addEventListener('click', () => {
+  navLink: function(el) {
+    el.addEventListener('click', function() {
       func.toggleMenu();
     });
   },
-  close: (e) => {
+  close: function(e) {
     func.toggleMenu();
   },
-  trackScroll: () => {
+  trackScroll: function() {
     const scrolled = window.pageYOffset;
     const coords = document.documentElement.clientHeight;
   
@@ -305,7 +28,7 @@ const func = {
       up.classList.remove('up-show');
     }
   },
-  backToTop: () => {
+  backToTop: function() {
     if (window.pageYOffset > 0) {
       window.scrollBy(0, -80);
       setTimeout(func.backToTop, 0);
@@ -318,3 +41,12 @@ navigationLink.forEach(func.navLink);
 popupClose.addEventListener('click', func.close);
 window.addEventListener('scroll', func.trackScroll);
 up.addEventListener('click', func.backToTop);
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+          callback.call(thisArg, this[i], i, this);
+      }
+  };
+}
---
+++ volga/js/slick-settings.js
@@ -0,0 +1,277 @@
+$(document).ready(function() {
+
+  $('.tariffs-slider').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    swipe: false,
+    prevArrow: '<div class="foundation-slider-prev"><svg class="foundation-slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    nextArrow: '<div class="foundation-slider-next"><svg class="foundation-slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    arrows: false,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false,
+          swipe: true,
+          dots: false,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          swipe: true,
+          dots: false,
+          arrows: true
+        }
+      }
+    ]
+  });
+  
+  $('.tariffs-slider-tabs').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    asNavFor: '.tariffs-slider',
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: "unslick"
+      }
+    ]
+  });
+  
+  
+  $('.cases-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    prevArrow: '<div class="cases-slider__prev"><svg class="cases-slider__icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    nextArrow: '<div class="cases-slider__next"><svg class="cases-slider__icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    responsive: [
+      {
+        breakpoint: 1335,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false,
+          arrows: false
+        }
+      }
+    ]
+  });
+  
+  $('.calculator-form__slider').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    prevArrow: '<div class="calculator-form__slider-prev"><svg class="calculator-form__slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    nextArrow: '<div class="calculator-form__slider-next"><svg class="calculator-form__slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    responsive: [
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 350,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false
+        }
+      }
+    ]
+  });
+  
+  $('.stages-slider').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    swipe: false,
+    arrows: false,
+    focusOnSelect: true
+  });
+  
+  $('.stages-slider-tabs').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.stages-slider',
+    focusOnSelect: true,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1110,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 530,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false
+        }
+      }
+    ]
+  });
+  
+  $('.feedback-slider').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    prevArrow: '<div class="feedback-slider-prev"><svg class="feedback-slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    nextArrow: '<div class="feedback-slider-next"><svg class="feedback-slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    responsive: [
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false
+        }
+      }
+    ]
+  });
+  
+  $('.certificates-slider').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    prevArrow: '<div class="certificates-slider-prev"><svg class="certificates-slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    nextArrow: '<div class="certificates-slider-next"><svg class="certificates-slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    responsive: [
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false
+        }
+      }
+    ]
+  });
+  
+  $('.foundation-slider').slick({
+    slidesToShow: 3,
+    prevArrow: '<div class="foundation-slider-prev"><svg class="foundation-slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    nextArrow: '<div class="foundation-slider-next"><svg class="foundation-slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false,
+          arrows: true 
+        }
+      }
+    ]
+  });
+  
+  $('.sale-slider').slick({
+    slidesToShow: 3,
+    prevArrow: '<div class="foundation-slider-prev"><svg class="foundation-slider-icon-prev"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    nextArrow: '<div class="foundation-slider-next"><svg class="foundation-slider-icon-next"><use xlink:href="img/sprite.svg#prev"></use></svg></div>',
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false,
+          arrows: true 
+        }
+      }
+    ]
+  });
+  
+});
