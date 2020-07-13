38323580f11a49061b60ab8094becf99744bfb28
Евгений <57830696+Salabaykin@users.noreply.github.com>
2020-07-14 00:28:09 +0300

    1zabor

--- 1zabor/js/main.js
+++ 1zabor/js/main.js
@@ -5,8 +5,28 @@ $('.gallery-slider').slick({
   prevArrow: '<div class="gallery-slider__prev"><svg class="gallery-slider__icon" width="14px" height="24px" viewBox="0 0 14 24" xmlns="http://www.w3.org/2000/svg"><polyline fill="none" stroke="#25588c" stroke-width="1.4" points="12.775,1 1.225,12 12.775,23 "></polyline></svg></div>',
   nextArrow: '<div class="gallery-slider__next"><svg width="14px" height="24px" viewBox="0 0 14 24" xmlns="http://www.w3.org/2000/svg"><polyline fill="none" stroke="#25588c" stroke-width="1.4" points="1.225,23 12.775,12 1.225,1 "></polyline></svg></div>',
   autoplay: true,
-  autoplaySpeed: 2000,
-  pauseOnHover: false
+  autoplaySpeed: 4000,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 770,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
 });
 
 (function($) {
@@ -60,3 +80,31 @@ const func = {
 window.addEventListener('scroll', func.trackScroll);
 up.addEventListener('click', func.backToTop);
 
+window.onscroll = function() {
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrolled !== 0) {
+    document.querySelector('.top-menu').classList.add('top-menu--fixed');
+  } else {
+    document.querySelector('.top-menu').classList.remove('top-menu--fixed');
+  }
+};
+
+(function() {
+  
+  const hamburger = {
+    navToggle: document.querySelector('.burger'),
+    nav: document.querySelector('.mob-nav__ul'),
+    doToggle: function() {
+      this.nav.classList.toggle('active');
+    }
+  };
+
+  document.addEventListener('click', e => {
+    if (e.target != hamburger.navToggle && e.target != hamburger.nav && e.target != document.querySelector('.mob-nav')) {
+      hamburger.nav.classList.remove('active');
+    } else {
+      hamburger.doToggle();
+    }
+  });
+  
+}());
